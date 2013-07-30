78867b32660f36e7d5042b939f4e718116730a1e
vinhyl <vincent.hui.xyl@gmail.com>
2013-07-30 17:04:49 +0800

    tutorial 3 finished

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader, RequestContext
+from polls.models import Poll
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, poll_id):
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/result.html', {'poll': poll})
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
